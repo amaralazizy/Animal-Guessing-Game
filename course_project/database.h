abb8ed5750a2fab397f9658b587e44d1afc19e6c
Abdullah Ayman <21-101131@students.eui.edu.eg>
2022-12-26 21:34:21 +0200

    comments for ammar

--- course_project/database.h
+++ course_project/database.h
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "data.h"
 
+//TODO: create a function to read the data from the file and return it as a string
+//TODO: create a function that takes as input a pointer of node and produces it as a string
+//TODO: create a function that takes as input the string returned from the file
+// and produces a pointer of node 
+
 void writeData() {
 	FILE* fp;
 
