104ba5343f72ae765c4f14f84e79fe529eac5a91
Amar Wael <21-101152.21-101152@students.eui.edu.eg>
2022-12-27 07:18:15 +0200

    Insert nodes to lines

--- course_project/mapping.h
+++ course_project/mapping.h
@@ -1,11 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 #include "data.h"
 #include "database.h"
 
-node* linesToNodes(line* currLine) {
-	node* currNode = (node*)(malloc(sizeof(node)));
+node *linesToNodes(line *currLine)
+{
+	node *currNode = (node *)(malloc(sizeof(node)));
 
-	if ((currLine->next)->text[0] == '\n') {
+	if ((currLine->next)->text[0] == '\n')
+	{
 		// this node is an animal
 		currNode->yes = NULL;
 		currNode->no = NULL;
@@ -13,6 +15,24 @@ node* linesToNodes(line* currLine) {
 	}
 }
 
-line* nodesToLines(node* currNode) {
-
+line *nodesToLines(node *currNode)
+{
+	line *currLine = (line *)(malloc(sizeof(line)));
+	currLine->text = currNode->name;
+	if ((!currNode->yes) && (!currNode->no))
+	{
+		// this node is an animal
+		currLine->next = (line *)(malloc(sizeof(line)));
+		(currLine->next)->text = '\n';
+		currLine->next->next = NULL;
+	}
+	else
+	{
+		currLine->next = nodesToLines(curNode->yes);
+		line *temp = NULL;
+		for (temp = currLine->next; temp->next != NULL; temp = temp->next)
+			;
+		temp->next = nodesToLines(curNode->no);
+	}
+	return currLine;
 }
