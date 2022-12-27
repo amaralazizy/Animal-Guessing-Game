8e7e1cf8db5fff79a37d2cdd636db5c2d13306d0
Abdullah Ayman <21-101131@students.eui.edu.eg>
2022-12-27 07:51:27 +0200

    lines to nodes function

--- course_project/mapping.h
+++ course_project/mapping.h
@@ -3,13 +3,45 @@
 #include "database.h"
 
 node* linesToNodes(line* currLine) {
-	node* currNode = (node*)(malloc(sizeof(node)));
+	static int numLines = 0;
 
-	if ((currLine->next)->text[0] == '\n') {
-		// this node is an animal
-		currNode->yes = NULL;
-		currNode->no = NULL;
+	numLines++;
+	if (currLine->text[0] == '\n') {
+		return linesToNodes(currLine->next);
+	}
+	else {
+		node* currNode = (node*)(malloc(sizeof(node)));
 		currNode->name = currLine->text;
+
+		if ((currLine->next)->text[0] == '\n') {
+			// this node is an animal
+			currNode->yes = NULL;
+			currNode->no = NULL;
+		}
+		else {
+			int currLineNum = numLines;
+			// this node is a question
+			currNode->yes = linesToNodes(currLine->next);
+
+			//line* temp1 = currLine->next->next;
+			//if (temp1->text[0] == '\n' && (temp1->next->next)->text[0] == '\n') {
+			//	// this is the last question in the branch
+			//	currNode->no = linesToNodes(temp1->next);
+			//}
+			//else {
+			//	int co = 1;
+
+			//	node* temp;
+			//	for (temp = currNode->yes->yes; temp != NULL; temp = temp->yes) co++;
+			//	for (temp = currNode->yes->no; temp != NULL; temp = temp->no) co++;
+
+			line* tempLine = currLine->next->next;
+			for (int i = 0; i < (numLines-currLineNum); i++) tempLine = tempLine->next;
+
+			currNode->no = linesToNodes(tempLine);
+			//}
+		}
+		return currNode;
 	}
 }
 
