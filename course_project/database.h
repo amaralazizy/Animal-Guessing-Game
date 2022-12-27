5772c0a92d0aa9c08b68ac540090806b0feb7f58
Amar Wael <21-101152.21-101152@students.eui.edu.eg>
2022-12-27 03:07:06 +0200

    Write over the file

--- course_project/database.h
+++ course_project/database.h
@@ -1,40 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
+#include <string.h>
 #include "data.h"
 
-
-line* filetext() {
-    FILE* ptr;
-    line* head = (line *)malloc(sizeof(line));
+line *filetext()
+{
+    FILE *ptr;
+    line *head = (line *)malloc(sizeof(line));
     head->next = NULL;
     char str[300];
     fopen_s(&ptr, "database.txt", "a+");
 
-    if (ptr == NULL) {
+    if (ptr == NULL)
+    {
         printf("file can't be opened \n");
     }
 
     printf("content of this file are \n");
     fgets(str, 300, ptr);
-	strcpy_s(head->text, 300, str);
+    strcpy_s(head->text, 300, str);
 
     int i = 1;
-    while (fgets(str, 300, ptr) != NULL) {
-		insertLine(i, str, &head);
-		i++;
+    while (fgets(str, 300, ptr) != NULL)
+    {
+        insertLine(i, str, &head);
+        i++;
     }
     fclose(ptr);
     return head;
 }
 
-void writeData()
+void writeData(line *lines)
 {
     FILE *fp;
 
     errno_t err;
 
-    if ((err = fopen_s(&fp, "database.txt", "w+")) != 0)
+    if ((err = fopen_s(&fp, "database.txt", "a")) != 0)
     {
         // This indicates that the file can not be opened
         // will print the error message
@@ -43,9 +45,9 @@ void writeData()
     }
     else
     {
-        fprintf(fp, "(\"does it live in water ?\"whale\" (\"Does it have four legs ? \" \"cat\" \"elephant\")) ");
-        // whale
-        // does it live in water
+        line *temp = filetext();
+        while (temp->next)
+            fprintf(fp, temp->text);
         fclose(fp);
     }
 }
